477078a3610bebf64e7484c755d56755f109dcd4
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-03-08 02:57:05 +0600

    MongoDB Connected For Database

--- index.js
+++ index.js
@@ -4,15 +4,76 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('colors');
 require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mogodb-practice.uoisaxb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 // API
 app.get("/", (req, res) => {
     res.send("Deshi Vibes Server Is Running...")
 })
+
+
+function verifyJWT(req, res, next) {
+    // const token = req.headers.authorization;
+    // console.log(token);
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send("UnAuthorized Access")
+    }
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+async function dataBase() {
+    try {
+        const usersCollection = client.db("deshi-vibes").collection("users");
+
+
+
+        // Save User Info
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            // const userEmail = user.email;
+            console.log(email);
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            }
+
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN);
+            res.send({ result, token });
+        })
+    }
+
+    catch (err) {
+        console.log(err.message.bgRed.bold)
+        console.log(err.stack.bgBlue.bold)
+    }
+}
+
+dataBase().catch(err => console.log(err.bold.bgRed));
 
 
 // Listen
